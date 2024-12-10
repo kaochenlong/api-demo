974daf58f0d6f5c27f237618a9fcdc3c203bf84b
Eddie Kao <eddie@5xcampus.com>
2024-12-10 10:53:31 +0800

    add ping test

--- __tests__/book.test.js
+++ __tests__/book.test.js
@@ -1,6 +1,11 @@
+import request from "supertest"
+import app from "../app.js"
+
 describe("Ping", () => {
-  it("will return pong", () => {
-    // GET /ping
-    // expect(結果) => {"message": "pong"}
+  it("will return pong", async () => {
+    const response = await request(app).get("/ping")
+
+    expect(response.body).toEqual({ message: "pong" })
+    expect(response.status).toBe(200)
   })
 })
