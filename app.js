788372ce1664a199ff0724ce99ce21e3a4b3b8ac
Eddie Kao <eddie@5xcampus.com>
2024-12-10 10:32:55 +0800

    move app to app.js

--- app.js
+++ app.js
@@ -1,16 +1,13 @@
 import Express from "express"
 import v1Route from "./routes/v1.js"
 
-const PORT = process.env.PORT || 3001
 const app = Express()
 app.use(Express.json())
-
-app.listen(PORT, () => {
-  console.log(`PORT: ${PORT} is on!!`)
-})
 
 app.get("/ping", (_, res) => {
   res.json({ message: "pong" })
 })
 
 app.use("/v1", v1Route)
+
+export default app
